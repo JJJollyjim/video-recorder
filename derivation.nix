7ef024a9d4f4b4fb78423cf60d21936c7f1940d0
Jamie McClymont <jamie@kwiius.com>
2020-03-20 22:43:55 +1300

    Use gitignoreSource

--- derivation.nix
+++ derivation.nix
@@ -1,10 +1,21 @@
 { pkgs, stdenv, ... }:
+let
+  gitignoreSrc = pkgs.fetchFromGitHub {
+    owner = "hercules-ci";
+    repo = "gitignore";
+    # put the latest commit sha of gitignore Nix library here:
+    rev = "2ced4519f865341adcb143c5d668f955a2cb997f";
+    # use what nix suggests in the mismatch message here:
+    sha256 = "sha256:0fc5bgv9syfcblp23y05kkfnpgh3gssz6vn24frs8dzw39algk2z";
+  };
+  inherit (import gitignoreSrc { inherit (pkgs) lib; }) gitignoreSource;
+in
 stdenv.mkDerivation rec {
   name = "video-recorder";
 
   buildInputs = [ pkgs.nodePackages.typescript ];
 
-  src = builtins.fetchGit ./.;
+  src = gitignoreSource ./.;
 
   buildPhase = ''
     tsc
